1a6b74ab9929eafd75f5e213f3532ed0acab23cf
triver <Altavista@87>
2020-09-28 13:08:05 +0530

    data seperated

--- src/test/java/stepDefinations/StepDef.java
+++ src/test/java/stepDefinations/StepDef.java
@@ -19,39 +19,24 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import pojo.AddPlace;
 import pojo.Location;
+import resources.TestDataBuilder;
 
 public class StepDef {
 	RequestSpecification res;
 	ResponseSpecification resspec;
 	Response response;
+	TestDataBuilder data = new TestDataBuilder();
 	
 		@Given("Add place payload")
 		public void add_place_payload() {
 			RestAssured.baseURI = "https://rahulshettyacademy.com";
-
-			AddPlace p = new AddPlace();
-			p.setAccuracy(50);
-			p.setAddress("29, side layout, cohen 09");
-			p.setLanguage("French-IN");
-			p.setPhone_number("(+91) 983 893 3937");
-			p.setWebsite("https://rahulshettyacademy.com");
-			p.setName("Frontline house");
-			List<String> myList = new ArrayList<String>();
-			myList.add("shoe park");
-			myList.add("shop");
-
-			p.setTypes(myList);
-			Location l = new Location();
-			l.setLat(-38.383494);
-			l.setLng(33.427362);
-			p.setLocation(l);
 
 			RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
 					.addQueryParam("key", "qaclick123").setContentType(ContentType.JSON).build();
 
 			 resspec = new ResponseSpecBuilder().expectStatusCode(200)
 					.expectContentType(ContentType.JSON).build();
-			res = given().spec(req).body(p);
+			res = given().spec(req).body(data.addPlacePayload());
 		}
 
 		@When("user called {string} using Post http call")
